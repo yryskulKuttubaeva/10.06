e8ce01fcac9ce0897c6ba19b6b484038d458bd99
yryskul <kuttubaevayryskul@gmail.com>
2022-06-12 15:38:47 +0600

    added com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [angle, setAngle] = useState(100);
+
+  useEffect(function() {
+        //Read
+         setAngle(localStorage.getItem('angle'))
+      }, []);
+
+
+  function onAngleChange({ target}) {
+    setAngle(+target.value)
+  }
+  const styles = {
+    width: "300px",
+    height:"300px",
+    backgroundColor:"red",
+    transform: `skew(${angle}deg)`,
+    transtion: `1s all ease-in`,
+    borderRadius: "50%",
+    
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div style={{marginBottom:"100px"}}>
+         <input type="range" min="0" max="360" value={angle} onChange={onAngleChange}/>
+       </div>
+       <div style={styles}></div>
     </div>
   );
 }
+export default App
 
-export default App;
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [number, setNumber] = useState(0);
+
+//   useEffect(function() {
+//     //Read
+//      setNumber(+localStorage.getItem('number'))
+//   }, []);
+  
+
+
+//   function onIncrease() {
+//     localStorage.setItem('number', number +1)
+//     setNumber(number+1)
+//   }
+
+//   function onDecrease() {
+//     localStorage.setItem('number', number -1)
+//     setNumber(number-1)
+//   }
+
+
+//   return (
+//     <div className="App">
+//       <button onClick={onIncrease}>+</button>
+//       {number ?? 0}
+//       <button onClick={onDecrease}>-</button>
+//     </div>
+//     );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from 'react';
+
+// function App() {
+// const [name, setName] = useState('');
+
+// useEffect(function() {
+//   //Read
+//    setName(localStorage.getItem(name))
+// }, []);
+
+// function onNameChange({target}) {
+//   //create/ Uptate
+//   localStorage.setItem('name', target.value);
+//   setName(target.value)
+
+// }
+// //delete
+// function onNameClear() {
+//   localStorage.setItem('name');
+// setName('')
+// }
+
+//   return (
+//     <div className="App">
+//       <input type="text"
+//       value={name}
+//       placeholder="Your name"
+//       onChange={onNameChange}/>
+//       <span>Your name is {name}</span>
+//       <button onClick={onNameClear}>Clear</button>
+//     </div>
+//   );
+// }
+
+// export default App;
